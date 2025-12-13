7cf40f37a7254307d641e40e55fdcccfe6a069c2
KavishDhiman <kavishdhiman2003@gmail.com>
2025-12-13 14:51:58 +0530

    celcuis to farenhiet

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Celcuis
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the temperature in celcuis:");
+        float celcius = float.Parse(Console.ReadLine());    
+        float faren = (celcius * 9/5) + 32;
+        Console.WriteLine("The temperature in Farenhiet is :"+faren);
+    }
+    
+
+}
